05f3c596a5fa2864b0c7c6eba08bab3f7317947e
lakersan2014 <lakersan2012@gmail.com>
2015-03-11 22:20:12 +0800

    update

--- src/main/java/com/xfxg99/sale/viewmodel/CustomerVM.java
+++ src/main/java/com/xfxg99/sale/viewmodel/CustomerVM.java
@@ -1,9 +1,15 @@
 package com.xfxg99.sale.viewmodel;
 
+import java.util.Date;
+import java.util.List;
+
 public class CustomerVM {
 	
 	private String id; 
 	private String name;
+	private String email;
+	private Date birthday;
+	private List<AddressVM>  addresses;
 	public String getId() {
 		return id;
 	}
@@ -15,6 +21,25 @@ public class CustomerVM {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    public List<AddressVM> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<AddressVM> addresses) {
+		this.addresses = addresses;
 	}
 	
 }
